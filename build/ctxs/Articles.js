a16ac83fd70f59ef0dbb7eda8e3666135813f65a
fiote <murilo.eng@gmail.com>
2022-02-18 01:49:36 -0300

    deleting build folder

--- build/ctxs/Articles.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const react_1 = require("react");
-const ArticlesContext = react_1.createContext({});
-exports.ArticlesProvider = ArticlesContext.Provider;
-exports.default = ArticlesContext;
